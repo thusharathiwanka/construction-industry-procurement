cf6d8ed96f5d4537b4b36a315a17c544d72f950b
Thushara Thiwanka <thusharathiwanka123@gmail.com>
2021-10-02 13:42:12 +0530

    CREATE: unit tests added
    for procurement officer, site manager

--- server/routes/index.routes.js
+++ server/routes/index.routes.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 
+router.use("/officers", require("./procurement.officer.routes"));
 router.use("/sitemanagers", require("./site.manager.routes"));
 router.use("/suppliers", require("./supplier.routes"));
 router.use("/users", require("./common.routes"));
