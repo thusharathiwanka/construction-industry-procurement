ae137bf35711d94c3efdc6ad1ba446fdeb3ace20
Thushara Thiwanka <thusharathiwanka123@gmail.com>
2021-10-04 23:18:11 +0530

    CREATE: services API created

--- server/routes/index.routes.js
+++ server/routes/index.routes.js
@@ -3,6 +3,7 @@ const router = require("express").Router();
 router.use("/officers", require("./procurement.officer.routes"));
 router.use("/sitemanagers", require("./site.manager.routes"));
 router.use("/suppliers", require("./supplier.routes"));
+router.use("/suppliers/services", require("./service.routes"));
 router.use("/users", require("./common.routes"));
 router.use("/orders", require("./order.routes"));
 router.use("/reports", require("./report.routes"));
