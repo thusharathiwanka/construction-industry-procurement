7130afe087db85a547c83291867eefcd8f1eeb8b
@chamindu.etcetera <it19079264@my.sliit.lk>
2021-10-05 09:11:55 +0530

    create inventory controller

--- client/src/pages/inventory.js
+++ client/src/pages/inventory.js
@@ -1,8 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Sidebar from "../components/sidebar/Sidebar";
 import TopNav from "../components/topnav/TopNav";
 
-const inventory = () => {
+const Inventory = () => {
+    
+    const [Inventory, setInventory] = useState({
+        item:"Sand",
+        max:""
+
+    })
+
     return (
         <div>
             <Sidebar/>
@@ -15,17 +22,57 @@ const inventory = () => {
                         <div className="col-8">
                             <div className="card">
                                 <div className="row ">
-                                    
+                                    {/* <div style={{display:'flex', justifyContent:'center', alignItems:'center', width:'100%'}}> */}
+                                    <div className="col-3"></div>
+                                    <div className="col-3">
+                                        <h3 style={{paddingTop:25}} >Select Item</h3>
+                                    </div>
+                                    <div className="col-3">
+                                        <div className="rowuser">
+											<select
+                                                    name="position"
+                                                    id="position"
+                                                    value={Inventory.item}
+                                                    onChange={(e)=>setInventory({...Inventory,item:e.target.value})}
+                                                    required
+                                                >
+                                                    <option value="sand">Sand</option>
+                                                    <option value="cement">Cement</option>
+                                                    <option value="stone">Stone</option>
+                                                    <option value="iron">Iron</option>
+                                                </select>
+										</div>
+                                    </div>
+                                    <div className="col-3"></div>
+                                    {/* </div> */}
                                 </div>
                                 <div className="row ">
-                                    
+                                     {/* <div style={{display:'flex', justifyContent:'center', alignItems:'center', width:'100%'}}> */}
+                                    <div className="col-3"></div>
+                                    <div className="col-3">
+                                        <h3 style={{paddingTop:25}}>Maximum Capacity</h3>
+                                    </div>
+                                    <div className="rowuser">
+                                        <div className="col-10">
+                                            <input
+												type="number"
+                                                min="1"
+												placeholder="Capacity"
+												value={Inventory.quantity}
+                                                onChange={(e)=>setInventory({...Inventory,quantity:e.target.value})}
+												required
+											/>
+                                        </div>
+                                    </div>
+                                    <div className="col-3"></div>
+                                    {/* </div> */}
                                 </div>
                                 <div style={{paddingTop:50}}>
                                     <div className="row ">
                                         <div style={{display:'flex', justifyContent:'center', alignItems:'center', width:'100%'}}>
                                             <div className="rowuser">
                                         <button type="submit " >
-                                            Add
+                                            Update
                                         </button>
                                     </div>
                                         </div>
@@ -41,4 +88,4 @@ const inventory = () => {
     )
 }
 
-export default inventory
+export default Inventory
