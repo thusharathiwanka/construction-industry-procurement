17f3f37c7711631c58a1b3477ede5e73c9e50668
@chamindu.etcetera <it19079264@my.sliit.lk>
2021-10-08 01:53:47 +0530

    popup.js created

--- server/controllers/goods.report.controller.js
+++ server/controllers/goods.report.controller.js
@@ -2,7 +2,23 @@ const GoodsReport = require("../models/goods.report.model")
 
 const saveGoodsReport = async(req, res) => {
     try{
-        const report = new GoodsReport(req.body)
+        const report = new GoodsReport({
+            orderId :req.body.orderId,
+            item :req.body.item,
+            quantity :req.body.quantity,
+            receivedDate :req.body.receivedDate,
+        })
+        await report.save();
+        res.status(200).json(report._id)
+    } catch (error) {
+		res.status(400);
+		console.log(error);
+	}
+}
+
+const updateGoodsReport = async(req, res) => {
+    try{
+        const report = new GoodsReport(req)
         await report.save();
         res.status(200).json(report._id)
     } catch (error) {
--- server/routes/order.routes.js
+++ server/routes/order.routes.js
@@ -1,15 +1,10 @@
 const router = require("express").Router();
-
 const OrderController = require("../controllers/order.controller");
 
 const { verifySiteManagerAuth } = require("../auth/site.manager.auth");
 const { verifySupplierAuth } = require("../auth/supplier.auth");
-const {
-	verifyProcurementOfficer,
-} = require("../auth/procurement.officer.auth");
-const {
-	verifyProcurementManagerAuth,
-} = require("../auth/procurement.manager.auth");
+const {verifyProcurementOfficer} = require("../auth/procurement.officer.auth");
+const {verifyProcurementManagerAuth} = require("../auth/procurement.manager.auth");
 
 router.post("/", verifySiteManagerAuth, OrderController.saveOrder);
 
@@ -22,7 +17,9 @@ router.get(
 );
 
 router.put("/", OrderController.allOrders);
+
 router.get("/getApproveOrders", OrderController.getApproveOrders);
+
 router.put(
 	"/officer/:id",
 	verifyProcurementOfficer,
