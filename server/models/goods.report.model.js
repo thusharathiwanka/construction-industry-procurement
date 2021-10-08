0a53fdc17a69fb8f63cb6d7c7f946ff545d32284
@chamindu.etcetera <it19079264@my.sliit.lk>
2021-10-09 01:58:08 +0530

    goodsReports finalized

--- server/models/goods.report.model.js
+++ server/models/goods.report.model.js
@@ -3,10 +3,12 @@ const mongoose = require("mongoose");
 const GoodsReportSchema = mongoose.Schema({
 	orderId: { type: mongoose.Schema.Types.ObjectId, ref: "orders" },
 	item: { type: String, required: true },
-	quantity: { type: Number, required: true },
-	review: { type: String, },
-	receivedDate: { type: Date, required: true },
-	description: { type: String, required: true },
+	agreed: { type: String },
+	quantity: { type: Number , required: true },
+	review: { type: String , required: true },
+	receivedDate: { type: String },
+	supplierId: {type:Object , required: true},
+	description: { type: String , required: true},
 });
 
 const GoodsReport = mongoose.model("goodsReports", GoodsReportSchema);
